01aa69495b555c7eaa9ecbb31675b340e171b4b3
Daeki <kimdaeki@gmail.com>
2021-07-23 16:58:36 +0900

    Initial commit

---
+++ src/com/iu/s1/object/ObjectMain1.java
@@ -0,0 +1,17 @@
+package com.iu.s1.object;
+
+public class ObjectMain1 {
+
+	public static void main(String[] args) {
+		//객체 생성
+		//클래스명 참조변수명 = new 생성자();
+		//Object 객체 생성
+		Object obj = new Object();
+		
+		//멤버변수, 멤버메서드 호출
+		//참조변수명.변수명
+		//참조변수명.메서드명([인자값]);
+
+	}
+
+}
