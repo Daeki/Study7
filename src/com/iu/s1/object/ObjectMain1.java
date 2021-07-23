9876bb68b519b1a3016edf1bb8294bc1bf47131f
Daeki <kimdaeki@gmail.com>
2021-07-23 17:55:18 +0900

    1

--- src/com/iu/s1/object/ObjectMain1.java
+++ src/com/iu/s1/object/ObjectMain1.java
@@ -11,6 +11,27 @@ public class ObjectMain1 {
 		//멤버변수, 멤버메서드 호출
 		//참조변수명.변수명
 		//참조변수명.메서드명([인자값]);
+		int num = obj.hashCode();
+		System.out.println(num);
+		
+		String str= obj.toString();
+		System.out.println(str);
+		System.out.println(obj);
+		
+		Object obj2 = new Object();
+		
+		boolean result = obj.equals(obj2);
+		System.out.println(obj == obj2);
+		System.out.println(result);
+		
+		Test test = new Test();
+		Test test2 = new Test();
+		System.out.println(test == test2);
+		System.out.println(test.num1==test2.num1);
+		System.out.println(test.equals(test2));
+//		test.hashCode();
+		
+//		Object obj3 = test;
 
 	}
 
---
+++ src/com/iu/s1/object/Test.java
@@ -0,0 +1,7 @@
+package com.iu.s1.object;
+
+public class Test {
+	int num1=1;
+	int num2=2;
+
+}
