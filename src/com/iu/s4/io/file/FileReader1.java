202f2b056ee278be1b863f8327b4ff8881c6ab6f
Daeki <kimdaeki@gmail.com>
2021-07-30 17:47:49 +0900

    Update FileReader1.java

--- src/com/iu/s4/io/file/FileReader1.java
+++ src/com/iu/s4/io/file/FileReader1.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class FileReader1 {
 
@@ -17,18 +18,34 @@ public class FileReader1 {
 			ArrayList<FileMember> ar = new ArrayList<FileMember>();
 			while(true) {
 				String str = br.readLine();
+				
 				if(str==null) {
 					break;
 				}
 				FileMember fileMember = new FileMember();
 				//파싱 작업
+				//1. split
+				String [] ar2 = str.split(",");
+				fileMember.setId(ar2[0]);
+				fileMember.setPw(ar2[1]);
+				fileMember.setAge(Integer.parseInt(ar2[2]));
+				
+				//2. StringTokenzier
+				StringTokenizer st = new StringTokenizer(str, ",");
+				fileMember.setId(st.nextToken());
+				fileMember.setPw(st.nextToken());
+				fileMember.setAge(Integer.parseInt(st.nextToken()));
+				
+				
+				ar.add(fileMember);
+				
 				System.out.println(str);
 			}
 			
 			for(int i=0;i<ar.size();i++) {
-				System.out.println("id : ");
-				System.out.println("Pw : ");
-				System.out.println("age : ");
+				System.out.println("id : "+ar.get(i).getId());
+				System.out.println("Pw : "+ar.get(i).getPw());
+				System.out.println("age : "+ar.get(i).getAge());
 			}
 			
 			
